4add2f716470cc42fa76b133d86308c3d952491f
Achmad Dicky Rizhaldi <37206097+dikyriz@users.noreply.github.com>
2022-02-25 01:11:24 +0700

    update progres page

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,7 @@
 import ImageBackground from './image/background-homes.jpg';
 import IconIg from './icon/icon-ig.png';
+import CardOne from './image/card-1.jpg';
+import CardTwo from './image/card-2.jpg';
+import CardThree from './image/card-3.jpg';
 
-export {ImageBackground, IconIg};
+export {ImageBackground, IconIg, CardOne, CardTwo, CardThree};
