c3f0385bf777cf08d166f019cfbdc5498fd76504
Achmad Dicky Rizhaldi <37206097+dikyriz@users.noreply.github.com>
2022-02-26 16:44:23 +0700

    update pages

--- src/pages/Pricing.js
+++ src/pages/Pricing.js
@@ -2,6 +2,10 @@ export default function Pricing () {
     return (
         <section className="section-pricing">
             <h1 className="pricing-title">Have a Project on Your Mind</h1>
+            <p className="pricing-description">Don't have a website yet ? Want to have a website for
+            product branding ?<span className="pricing-description-child">Confused where to do
+            it. Trust us to build your website.</span> 
+            </p>
         </section>
     );
 }
