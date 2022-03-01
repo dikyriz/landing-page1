ec5c9ab65c4fbb30ad037267f28318107083586a
Achmad Dicky Rizhaldi <37206097+dikyriz@users.noreply.github.com>
2022-03-01 11:18:01 +0700

    add image

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,5 +3,8 @@ import IconIg from './icon/icon-ig.png';
 import CardOne from './image/card-1.jpg';
 import CardTwo from './image/card-2.jpg';
 import CardThree from './image/card-3.jpg';
+import CardOneBlog from './image/card-1-blog.jpg';
+import CardTwoBlog from './image/card-2-blog.jpg';
+import CardThreeBlog from './image/card-3-blog.jpg';
 
-export {ImageBackground, IconIg, CardOne, CardTwo, CardThree};
+export {ImageBackground, IconIg, CardOne, CardTwo, CardThree, CardOneBlog, CardTwoBlog, CardThreeBlog};
