afc99ba584e1e765cf473cc2b141de62892c5035
Achmad Dicky Rizhaldi <37206097+dikyriz@users.noreply.github.com>
2022-03-01 12:42:39 +0700

    update

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,8 +1,25 @@
+import { CardOneBlog } from "../assets";
+
 export default function Blog () {
     return (
         <section className="section-blog">
-            <h1 className="blog-title">Blog Posts</h1>
-            <p className="blog-description">Latest news and articles</p>
+            <div className="header-blog">
+                <h1 className="blog-title">Blog Posts</h1>
+                <p className="blog-description">Latest news and articles</p>
+            </div>
+            <div className="card-blog">
+                <div className="card-1-blog">
+                    <img src={CardOneBlog} alt="card-1-blog" className="img-blog"/>
+                    <div className="container-blog">
+                    <h4>Stages of Making a Vi..</h4>
+                    <p>Lorem dolor sit amet, consectetur 
+                    adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna 
+                    aliqua ....
+                    </p>
+                    </div>
+                </div>
+            </div>                
         </section>
     );
 }
