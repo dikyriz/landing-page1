1a2b6c5542b91e571cdc4bd52225ea188e9a8ade
Achmad Dicky Rizhaldi <37206097+dikyriz@users.noreply.github.com>
2022-03-02 21:11:52 +0700

    update style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ export default function Home () {
             <h4 className="home-title2">I'am a Product Design and<span className="home-title-child2">Fullstack Website Developer</span></h4>
 
             <button className="btn btn-contact"><Link to='/about'>Contact Me<span className="arrow">&#8722;&gt;</span></Link></button>
-            <button className="btn btn-portofolio"><Link to='/portofolio'>Portofolio<span className="arrow">&#8722;&gt;</span></Link></button>
+            <Link to='/portofolio' className="btn btn-portofolio">Portofolio<span className="arrow">&#8722;&gt;</span></Link>
             <p className="home-sosmed">
                 Dribble | Github | Instagram
             </p>
