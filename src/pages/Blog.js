38fb60727838bc79a8142380a4ba8fc7abec103f
Achmad Dicky Rizhaldi <37206097+dikyriz@users.noreply.github.com>
2022-03-16 08:45:55 +0700

    update pages

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -3,7 +3,8 @@ import './css/blog.css';
 
 export default function Blog () {
     return (
-        <section className="section-blog" id="blog">
+        <section className="section-blog">
+            <a className="anchor" id="blog" />
             <div className="header-blog">
                 <h1 className="blog-title">Blog Posts</h1>
                 <p className="blog-description">Latest news and articles</p>
