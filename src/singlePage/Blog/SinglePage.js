3c0a0ea58ecdb9bb52ae12a0e6f2f9bf6e48f3be
Achmad Dicky Rizhaldi <37206097+dikyriz@users.noreply.github.com>
2022-03-19 21:55:09 +0700

    add topbar and setting componen in file

--- src/singlePage/Blog/SinglePage.js
+++ src/singlePage/Blog/SinglePage.js
@@ -1,13 +1,15 @@
 import Footer from "../../pages/Footer";
 import DetailBlog from "./DetailBlog";
+import Blogs from "./Blogs";
 import Header from "./Header";
+import TopBarSP from "../topBarSP/TopBarSP";
 
 export default function SinglePage() {
     return (
         <section>
+            <TopBarSP/>
            <Header/>
-           <DetailBlog/>
-           <Footer/>
+           <Blogs/>
         </section>
     );
 }
