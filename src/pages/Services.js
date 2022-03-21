9a58bd2b723538e07c2766b2c42abfccd8ffc190
Achmad Dicky Rizhaldi <37206097+dikyriz@users.noreply.github.com>
2022-03-21 12:19:45 +0700

    update pages and style Services

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -13,39 +13,42 @@ export default function Services () {
                 <span className = "services-description-child" > innovation and creative perspectives.</span> 
                 </p>
                 <div className = "card-services" >
-                    <ul>       
-                    <li>
-                    <div className = "card-1-services" >
-                    <img src = {CardThree} alt = "card-3" className = "img-services" />
-                        <div className = "container-services" >
-                            <h4 className = "card-title-services-card1" > Web Design </h4> 
+                    <ul className="flip-box">       
+                    <li className="flip-box-inner">
+                        <div className="flip-box-front">
+                            <h4 className = "card-title-services-card1" > Web Design </h4>
+                        </div>
+                        <div className="flip-box-back">
                             <p className = "card-description-services-card1" >
                             Making plans wireframe to UI design on demand and does not neglect the main
-                            function. </p> 
-                        </div> 
-                    </div> 
-                    </li> 
-                    <li>
-                    <div className = "card-2-services" >
-                        <div className = "container-services" >
-                            <h4 className = "card-title-services" > Web Development </h4> 
-                            <p className = "card-description-services" >
-                            Make a website display according to the results of slicing UI design and create and design database and systems can running smoothly. 
-                            </p> 
-                        </div> 
-                    </div> 
-                    </li> 
-                    <li >
-                    <div className = "card-3-services" >
-                        <div className = "container-services" >
-                            <h4 className = "card-title-services" > App Development </h4> 
+                            function. </p>
+                        </div>                    
+                    </li>
+                    </ul>  
+                    <ul className="flip-box">
+                    <li className="flip-box-inner">
+                        <div className="flip-box-front">
+                            <h4 className = "card-title-services" > Web Development </h4>
+                        </div>
+                        <div className="flip-box-back">
+                             <p className = "card-description-services" >
+                             Make a website display according to the results of slicing UI design and create and design database and systems can running smoothly. 
+                            </p>
+                        </div>                
+                    </li>
+                    </ul>
+                    <ul className="flip-box ">
+                    <li className="flip-box-inner">
+                        <div className="flip-box-front">
+                            <h4 className = "card-title-services" > App Development </h4>
+                        </div>
+                        <div className="flip-box-back">
                             <p className = "card-description-services" >
                             Mobile application development to make work easier and certainly can be used wherever you are. 
-                            </p> 
-                        </div> 
-                    </div> 
-                    </li> 
-                    </ul> 
+                            </p>
+                        </div>
+                    </li>
+                    </ul>                     
                 </div>
             </div>
             <div className="section-pricing">
